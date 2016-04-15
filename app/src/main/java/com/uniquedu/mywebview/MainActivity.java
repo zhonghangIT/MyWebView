42b70ace161ee5d9759880c6c7ff1347d007ba37
ZhongHang <zhonghangIT@163.com>
2016-04-15 13:28:30 +0800

    Initial commit

---
+++ app/src/main/java/com/uniquedu/mywebview/MainActivity.java
@@ -0,0 +1,73 @@
+package com.uniquedu.mywebview;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @InjectView(R.id.button_load)
+    Button buttonLoad;
+    @InjectView(R.id.button_js)
+    Button buttonJs;
+    @InjectView(R.id.progressbar)
+    ProgressBar progressbar;
+    @InjectView(R.id.webview)
+    WebView webview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
+        webview.getSettings().setJavaScriptEnabled(true);//支持js
+        webview.addJavascriptInterface(new MyJs(), "myJsObj");//设置坚挺js中方法的对象
+        //监听html上的js方法的调用，不添加webview不会弹出alert
+        webview.onKeyDown(WebView.KEEP_SCREEN_ON,null);
+        webview.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+                //添加网页加载进度
+                if (newProgress == 100) {
+                    progressbar.setVisibility(View.GONE);
+                } else {
+                    if (progressbar.getVisibility() == View.GONE)
+                        progressbar.setVisibility(View.VISIBLE);
+                    progressbar.setProgress(newProgress);
+                }
+            }
+        });
+    }
+
+    @OnClick({R.id.button_load, R.id.button_js})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.button_load:
+                webview.loadUrl("file:///android_asset/index.html");
+                break;
+            case R.id.button_js:
+                webview.loadUrl("javascript:myAlert()");
+                break;
+        }
+    }
+
+    class MyJs {
+        //4.4以后必须添加这个注解，不然事件无响应
+        @JavascriptInterface
+        public void useAndroidMethod(String content) {
+            Toast.makeText(getApplicationContext(), "调用到android的方法" + content, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
